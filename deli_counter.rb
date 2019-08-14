0491a4618c26142ce2a9adcb85252d29dd3733d8
Eric Loudon <loudon.eric@gmail.com>
2019-08-14 08:28:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,4 +3,8 @@ def line(line)
 
 katz_deli = []
 
-if 
+if line.size == 0
+  puts "The line is currently empty."
+else
+  line,each_with_index do |person, index|
+    katz_deli.push ("#{index + 1}. #{person}")
