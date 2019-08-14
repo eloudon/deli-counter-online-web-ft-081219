0c630573f6f85b095b0412e7f88ce93020f5e6e4
Eric Loudon <loudon.eric@gmail.com>
2019-08-14 08:26:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,18 +1,4 @@
 # Write your code here.
 katz_deli = []
 
-def line(line)
-  if line.size = 0
-    puts "The line is currently empty."
-
-  else
-    ct = "The line is currently: "
-    count = 0
-    line.each do |person|
-      count += 1
-      ct += "#{count}. #{person}"
-      ct += " " if count !=line.size
-    end
-    puts ct
-    ct
-  end
+def line(ary)
