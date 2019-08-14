368f3b2cdb989915b21291b59dad4de54674feb8
Eric Loudon <loudon.eric@gmail.com>
2019-08-14 08:20:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+katz_deli = []
+
+def line(line)
+  if line.size = 0 
+    puts "The line is currently empty."
+
+  else 
+    current = "The line is currently: "
+    count = 0 
+    line.each do |person|
+      count += 1
+      
