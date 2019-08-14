756e00cea839a63659d68458111c29c6502c5494
Eric Loudon <loudon.eric@gmail.com>
2019-08-14 08:31:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,19 +1,15 @@
 # Write your code here.
 def line(katz_deli)
-  final_array = []
-
+  ary = []
    if katz_deli.size == 0
     puts "The line is currently empty."
   else
     katz_deli.each_with_index do |person, index|
-      final_array.push("#{index + 1}. #{person}")
-
+    ary.push("#{index + 1}. #{person}")
     end
-    puts "The line is currently: " + final_array.join(" ")
+    puts "The line is currently: " + ary.join(" ")
   end
-
  end
-
 
 
  def take_a_number(katz_deli, customer)
@@ -21,6 +17,7 @@ def line(katz_deli)
   position = katz_deli.size
   puts "Welcome, #{customer}. You are number #{position} in line."
 end
+
 
 
  def now_serving(katz_deli)
